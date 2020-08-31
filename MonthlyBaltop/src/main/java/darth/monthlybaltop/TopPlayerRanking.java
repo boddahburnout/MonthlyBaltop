e5d3fcd8ce2ae8b19b9053b898197591b8dfdae7
DiamondDagger590 <diamonddagger590@gmail.com>
2020-08-31 10:25:19 -0500

    Invert ranking

--- MonthlyBaltop/src/main/java/darth/monthlybaltop/TopPlayerRanking.java
+++ MonthlyBaltop/src/main/java/darth/monthlybaltop/TopPlayerRanking.java
@@ -25,17 +25,17 @@ public class TopPlayerRanking implements Comparable{
     if(o instanceof TopPlayerRanking){
       TopPlayerRanking topPlayerRanking = (TopPlayerRanking) o;
       if(topPlayerRanking.getBalance() > this.getBalance()){
-        return 1;
+        return -1;
       }
       else if(topPlayerRanking.getBalance() < this.getBalance()){
-        return -1;
+        return 1;
       }
       else {
         return 0;
       }
     }
     else{
-      return -1;
+      return 1;
     }
   }
 }
