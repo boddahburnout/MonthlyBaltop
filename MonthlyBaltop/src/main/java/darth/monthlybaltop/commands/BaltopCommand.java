5a38aec161f96b126aca3540cc97ac5a5b2bac47
Dakota Vega <dvega9876@gmail.com>
2020-09-03 21:21:40 -0400

    Bug fixes
    Fixed cache updating issue, Reduced to top 8, and added error handling to cache system

--- MonthlyBaltop/src/main/java/darth/monthlybaltop/commands/BaltopCommand.java
+++ MonthlyBaltop/src/main/java/darth/monthlybaltop/commands/BaltopCommand.java
@@ -20,9 +20,9 @@ import java.util.List;
 import java.util.UUID;
 
 public class BaltopCommand implements Listener {
-    
+
     final private MonthlyBaltop plugin;
-    
+
     private static List<TopPlayerRanking> topPlayerRankings = new ArrayList<>();
 
     public BaltopCommand(MonthlyBaltop conf) {
@@ -31,6 +31,7 @@ public class BaltopCommand implements Listener {
         new BukkitRunnable(){
             @Override
             public void run(){
+                plugin.cfgm.reloadMonthData();
                 List<TopPlayerRanking> newList = new ArrayList<>();
 
                 for(TopPlayerRanking topPlayerRanking : topPlayerRankings){
@@ -42,9 +43,9 @@ public class BaltopCommand implements Listener {
                 newList.sort(TopPlayerRanking::compareTo);
                 topPlayerRankings = newList;
             }
-        }.runTaskTimer(conf, 5 * 60 * 20, 5 * 20 * 60);
+        }.runTaskTimer(conf, 1 * 60 * 20, 1 * 20 * 60);
     }
-    
+
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onCommandPreProcess(PlayerCommandPreprocessEvent event) {
         plugin.loadConfigManager();
@@ -55,40 +56,43 @@ public class BaltopCommand implements Listener {
                 event.setCancelled(true);
             }
             else {
-                
+
                 event.setCancelled(true);
-                        event.getPlayer().sendMessage("");
-                        event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('$', plugin.getConfig().getString("header")));
-                        PlayerNameCache playerNameCache = plugin.getPlayerNameCache();
+                event.getPlayer().sendMessage("");
+                event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('$', plugin.getConfig().getString("header")));
+                PlayerNameCache playerNameCache = plugin.getPlayerNameCache();
 
-                        //If we haven't cached top ranked players
-                        if(topPlayerRankings.isEmpty() || plugin.cfgm.isMarkedForChange()){
+                //If we haven't cached top ranked players
+                try {
+                    if (topPlayerRankings.isEmpty() || plugin.cfgm.isMarkedForChange()) {
 
-                            String key = currentdate.getMonth().toString() + "-" + currentdate.getYear();
+                        String key = currentdate.getMonth().toString() + "-" + currentdate.getYear();
 
-                            for(String temp : plugin.cfgm.getMonthData().getConfigurationSection(key).getKeys(false)){
+                        for (String temp : plugin.cfgm.getMonthData().getConfigurationSection(key).getKeys(false)) {
 
-                                UUID playerUUID = UUID.fromString(temp);
+                            UUID playerUUID = UUID.fromString(temp);
 
-                                double startbal = (double) plugin.cfgm.getMonthData().get(key + "." + temp);
+                            double startbal = (double) plugin.cfgm.getMonthData().get(key + "." + temp);
 
-                                //event.getPlayer().sendMessage(""+MonthlyBaltop.getEconomy().getBalance(Bukkit.getOfflinePlayer(playerUUID)));
+                            double monthbal = MonthlyBaltop.getEconomy().getBalance(Bukkit.getOfflinePlayer(playerUUID)) - startbal;
 
-                                double monthbal = MonthlyBaltop.getEconomy().getBalance(Bukkit.getOfflinePlayer(playerUUID)) - startbal;
-                        
-                                topPlayerRankings.add(new TopPlayerRanking(playerUUID, monthbal, startbal));
+                            topPlayerRankings.add(new TopPlayerRanking(playerUUID, monthbal, startbal));
+                        }
                     }
-                    
-                    topPlayerRankings.sort(TopPlayerRanking::compareTo);
-                    
-                    plugin.cfgm.unmarkForChange();
+
+                } catch (IllegalArgumentException error) {
+
                 }
-                
+                    topPlayerRankings.sort(TopPlayerRanking::compareTo);
+
+                    plugin.cfgm.unmarkForChange();
+
+
                 //Loop through top 10 or if there are less ranking than 10, then all rankings and display them
-                for(int i = 0; i < Math.min(10, topPlayerRankings.size()); i++){
-                    
+                for(int i = 0; i < Math.min(8, topPlayerRankings.size()); i++){
+
                     TopPlayerRanking topPlayerRanking = topPlayerRankings.get(i);
-                    
+
                     String playerName = playerNameCache.getName(topPlayerRanking.getUuid());
                     event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes ('$', plugin.getConfig().getString("number-color")) + (i + 1) + ") " + ChatColor.translateAlternateColorCodes ('$', plugin.getConfig().getString("name-color")) + playerName + ChatColor.translateAlternateColorCodes ('$', plugin.getConfig().getString("money-color")) + ", $" + String.valueOf(round(topPlayerRanking.getBalance(), 2)));
                 }
